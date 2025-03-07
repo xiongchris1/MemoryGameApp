160322f927ffc4fb27245766a8200bf4d94408b5
Chris Xiong <xiongchris1@gmail.com>
2025-03-07 09:47:01 -0700

    Initial Commit

---
+++ MemoryGameApp/MemoryGameAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameAppApp.swift
+//  MemoryGameApp
+//
+//  Created by Chris Xiong on 3/7/25.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
